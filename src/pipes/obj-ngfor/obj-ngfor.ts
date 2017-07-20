548614de13500036398aaf9bf6fde7fa07d8d051
Alejandro Quadri <alejandroquadri@gmail.com>
2017-07-20 17:40:28 -0300

    detalles de css

--- src/pipes/obj-ngfor/obj-ngfor.ts
+++ src/pipes/obj-ngfor/obj-ngfor.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ObjNgforPipe implements PipeTransform {
   transform(value: any, args: any[] = null): any {
-	  	console.log(Object.keys(value))
+	  	// console.log(Object.keys(value))
       return Object.keys(value)//.map(key => value[key]);
     }
 }
