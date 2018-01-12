7442e823a89eb002ff613160e5e1696c5ef173b0
Alejandro Quadri <alejandroquadri@gmail.com>
2018-01-12 13:46:39 -0300

    saque los calls de ionic native dado que esto va a ser una PWA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { Platform, Nav, Content } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+// import { StatusBar } from '@ionic-native/status-bar';
+// import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { AuthDataProvider, SplitShowProvider, SparePartsDataProvider } from '../providers';
 
@@ -21,8 +21,8 @@ export class MyApp {
 
   constructor(
     public platform: Platform,
-    public statusBar: StatusBar,
-    public splashScreen: SplashScreen,
+    // public statusBar: StatusBar,
+    // public splashScreen: SplashScreen,
     private splitShow: SplitShowProvider,
     private authData: AuthDataProvider,
     private spareParts: SparePartsDataProvider
@@ -30,8 +30,8 @@ export class MyApp {
     platform.ready().then(() => {
         // Okay, so the platform is ready and our plugins are available.
         // Here you can do any higher level native things you might need.console.log('did load');
-      statusBar.styleDefault();
-      splashScreen.hide();
+      // statusBar.styleDefault();
+      // splashScreen.hide();
       authData.user.subscribe( user => {
         console.log(user);
         this.userProfile = user;
